997ebed89be9589f007bc5ed718d5f6bfadf361a
unknown <fadlullahyazid@gmail.com>
2019-09-24 23:12:27 +0700

    First commit

---
+++ src/pbo11k/pkg10118907/latihan3/input/PBO11K10118907Latihan3Input.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo11k.pkg10118907.latihan3.input;
+
+import java.util.Scanner;
+/**
+ *
+ * @author
+ * NAMA     : Yazid Fadlullah At-Taukily
+ * KELAS    : PBO11K
+ * NIM      : 10118907
+ * Deskripsi Program : Latihan inputan dari keyboard
+ * 
+ */
+public class PBO11K10118907Latihan3Input {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+    System.out.print("Masukkan nama Anda: ");
+    Scanner scanner = new Scanner(System.in);
+
+    String nama = scanner.next();
+    System.out.println("Nama Anda adalah "+nama);
+    }
+    
+}
